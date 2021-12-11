3f1314839bdde465d3c361fbf3a5be3770625230
unknown <10.javierprieto.1a@gmail.com>
2021-12-11 12:55:42 +0100

    many new changes

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,21 @@
-import '../styles/globals.css'
+import '../styles/globals.css';
 import Navbar from '../components/Navbar'
 import Footer from '../components/Footer';
+import React from 'react';
+
+
 
 
 
 function MyApp({ Component, pageProps }) {
 
-  
+  React.useEffect(() => {
+    // Remove the server-side injected CSS para que cargue el css de MUI antes!
+    const jssStyles = document.querySelector('#jss-server-side');
+    if (jssStyles) {
+      jssStyles.parentElement.removeChild(jssStyles);
+    }
+  }, []);
 
   return (
     <>
